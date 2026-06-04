7566f01f21e991e414398e1782b7752f3c66bd20
purple grape <purplegrape4@gmail.com>
2026-06-04 18:57:59 +0800

    update 8.4

--- 8.4/entrypoint.sh
+++ 8.4/entrypoint.sh
@@ -6,10 +6,6 @@ if [ -f Caddyfile ]; then
     frankenphp fmt --overwrite Caddyfile
 fi
 
-if [ -z "$FRANKENPHP_CONFIG" ]; then
-    export FRANKENPHP_CONFIG="num_threads 2;max_threads 4"
-fi
-
 if [ ! -d /app/public ]; then
     mkdir -p /app/public
     chown -R frankenphp:frankenphp /app/public
