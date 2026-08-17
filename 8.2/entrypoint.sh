e3bc9574fe68c6c4fb3ff394dabb95613af16a40
purple grape <purplegrape4@gmail.com>
2026-08-18 00:51:57 +0800

    update entrypoint.sh

--- 8.2/entrypoint.sh
+++ 8.2/entrypoint.sh
@@ -2,6 +2,19 @@
 
 set -e
 
+XDG_CONFIG_HOME=/app/.config
+XDG_DATA_HOME=/app/.local/share
+
+if [ ! -d /app/.config ]; then
+    mkdir -p /app/.config
+    chown -R frankenphp:frankenphp /app/.config
+fi
+
+if [ ! -d /app/.local/share ]; then
+    mkdir -p /app/.local/share
+    chown -R frankenphp:frankenphp /app/.local
+fi
+
 if [ -f Caddyfile ]; then
     frankenphp fmt --overwrite Caddyfile
 fi
